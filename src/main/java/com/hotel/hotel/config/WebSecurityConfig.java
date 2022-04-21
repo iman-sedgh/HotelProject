6f2170f951e2923e785d3317d1c6ce18207ddced
mahdi <MahdiGhorbaniMQ@gmail.com>
2022-04-21 17:20:10 +0430

    security and mvc configs added

---
+++ src/main/java/com/hotel/hotel/config/WebSecurityConfig.java
@@ -0,0 +1,88 @@
+package com.hotel.hotel.config;
+
+import com.hotel.hotel.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/anonymous/**").anonymous()
+                .antMatchers("/register", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("username")
+//                .loginPage("/login")
+                .defaultSuccessUrl("/greeting", true)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+
+
+    }
+
+//    @Override
+//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+////        auth.inMemoryAuthentication()
+////                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
+////                .and()
+////                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
+////                .and()
+////                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+////        auth.userDetailsService(userDetailsService);
+////        auth.
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+
+
+
+}
