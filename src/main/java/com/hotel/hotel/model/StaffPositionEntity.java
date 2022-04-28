47e27b703729f354baba38666c5f339e0d91ec01
AmirPouyaAmiri <seyedamirpouyaamiri@gmail.com>
2022-04-29 02:57:09 +0430

    insert id & position name and hotel FK for StaffPositionEntity

--- src/main/java/com/hotel/hotel/model/StaffPositionEntity.java
+++ src/main/java/com/hotel/hotel/model/StaffPositionEntity.java
@@ -1,4 +1,24 @@
 package com.hotel.hotel.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class StaffPositionEntity {
+
+    @Id
+    @Getter @Setter
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Getter @Setter
+    private String positionName;
+
+    @ManyToOne
+    private HotelEntity hotel;
+
 }
