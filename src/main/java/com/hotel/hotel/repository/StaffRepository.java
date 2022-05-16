f7974659f0999357df389e5f2bcee26863b2ae6e
AmirPouyaAmiri <seyedamirpouyaamiri@gmail.com>
2022-05-17 01:59:16 +0430

    create staffRepository

---
+++ src/main/java/com/hotel/hotel/repository/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.hotel.hotel.repository;
+
+import com.hotel.hotel.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface StaffRepository extends JpaRepository<UserEntity, Integer> {
+
+}
