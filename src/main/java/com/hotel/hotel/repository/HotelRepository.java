58a48520f2c2574663af3518194a6cc6dc62afd1
iman-sedgh <iman.sedgh@yahoo.com>
2022-06-15 17:05:51 +0430

    Add calcProfit costum Query to calculate a hotel whole Profit

--- src/main/java/com/hotel/hotel/repository/HotelRepository.java
+++ src/main/java/com/hotel/hotel/repository/HotelRepository.java
@@ -2,11 +2,17 @@ package com.hotel.hotel.repository;
 
 import com.hotel.hotel.model.HotelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface HotelRepository extends JpaRepository<HotelEntity,Integer> {
     Collection<HotelEntity> findAllByCity(String city);
     Collection<HotelEntity> findAllByOwner(String username);
     HotelEntity findFirstByOrderByIdDesc();
+    @Query(value = "SELECT SUM(s.salary), SUM(r.price) FROM StaffEntity s, ReservationEntity r " +
+            "where hotel_id= :hotelID" )
+    List<Integer> calcProfit(@Param("hotelID") Integer hotelID);
 }
